71640df92a76d5b330e856cb46973efd98e6c769
devnine99 <46771072+devnine99@users.noreply.github.com>
2025-12-16 15:19:39 +0900

    main

--- src/app/department/admin.py
+++ src/app/department/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 
 from app.department.models import Department
+from app.sub_department.models import SubDepartment
+
+
+class SubDepartmentInline(admin.TabularInline):
+    model = SubDepartment
+    extra = 0
 
 
 @admin.register(Department)
 class DepartmentAdmin(admin.ModelAdmin):
+    inlines = [SubDepartmentInline]
     list_display = ["name"]
     search_fields = ["name"]
     search_help_text = "분과명으로 검색하세요."
