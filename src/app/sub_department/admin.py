1a340818faf7626ebc4c05637fe2058df787247f
devnine99 <46771072+devnine99@users.noreply.github.com>
2025-12-16 15:23:55 +0900

    main

--- src/app/sub_department/admin.py
+++ src/app/sub_department/admin.py
@@ -1,8 +1,9 @@
-# from django.contrib import admin
-#
-# from app.sub_department.models import SubDepartment
-#
-#
-# @admin.register(SubDepartment)
-# class SubDepartmentAdmin(admin.ModelAdmin):
-#     pass
+from django.contrib import admin
+
+from app.sub_department.models import SubDepartment
+
+
+@admin.register(SubDepartment)
+class SubDepartmentAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+    raw_id_fields = ["department"]
