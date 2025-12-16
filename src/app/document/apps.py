9c9b143e868d69e38df26781adae96712d3ac3db
devnine99 <46771072+devnine99@users.noreply.github.com>
2025-12-16 21:54:12 +0900

    main

--- src/app/document/apps.py
+++ src/app/document/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class DocumentConfig(AppConfig):
     name = "app.document"
-    verbose_name = "14.자료"
+    verbose_name = "15.자료"
 
     def ready(self):
         import app.document.signals
