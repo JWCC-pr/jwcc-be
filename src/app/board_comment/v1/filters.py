eba2a187ba10d497232a42edd294a35390fab3b7
양진혁 <jhyang@yangjinhyeog-ui-MacBookAir.local>
2025-12-17 16:03:59 +0900

    hotfix: 게시판 댓글 이슈 대웅

--- src/app/board_comment/v1/filters.py
+++ src/app/board_comment/v1/filters.py
@@ -1,7 +1,14 @@
 import django_filters
 
+from app.board.models import Board
 from app.board_comment.models import BoardComment
 
 
 class BoardCommentFilter(django_filters.FilterSet):
     parent_id = django_filters.ModelChoiceFilter(label="부모 댓글 ID", queryset=BoardComment.objects.all())
+
+    def filter_queryset(self, queryset):
+        queryset = super().filter_queryset(queryset)
+        if self.request.query_params.get("parent_id") is None:
+            queryset = queryset.filter(parent__isnull=True)
+        return queryset
