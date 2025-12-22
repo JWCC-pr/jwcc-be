3975236bfc1caecfd386b0142819641e163977af
devnine99 <46771072+devnine99@users.noreply.github.com>
2025-12-22 10:54:09 +0900

    presigned url

--- src/app/presigned_url/v1/serializers.py
+++ src/app/presigned_url/v1/serializers.py
@@ -2,25 +2,32 @@ from django.apps import apps
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
+from app.common.storages import PublicMediaStorage
 from app.presigned_url.utils import FIELD_CHOICES
 
 
 class PresignedSerializer(serializers.Serializer):
-    field_choice = serializers.ChoiceField(choices=FIELD_CHOICES, write_only=True)
+    field_choice = serializers.ChoiceField(choices=FIELD_CHOICES, write_only=True, required=False)
     file_name = serializers.CharField(write_only=True)
     is_download = serializers.BooleanField(write_only=True)
     url = serializers.URLField(read_only=True)
     fields = serializers.JSONField(read_only=True)
 
     def validate(self, attrs):
-        model, field_name = self.get_field_info(attrs.get("field_choice"))
-        field = model._meta.get_field(field_name)
-        if callable(field.upload_to):
-            upload_to = field.upload_to(None, attrs["file_name"])
+        if attrs.get("field_choice"):
+            model, field_name = self.get_field_info(attrs["field_choice"])
+            field = model._meta.get_field(field_name)
+            if callable(field.upload_to):
+                upload_to = field.upload_to(None, attrs["file_name"])
+            else:
+                upload_to = field.upload_to
+            response = field.storage.generate_presigned_post(f"{upload_to}{attrs['file_name']}", attrs["is_download"])
+            attrs.update(response)
         else:
-            upload_to = field.upload_to
-        response = field.storage.generate_presigned_post(f"{upload_to}{attrs['file_name']}", attrs["is_download"])
-        attrs.update(response)
+            response = PublicMediaStorage().generate_presigned_post(
+                f"upload/{attrs['file_name']}", attrs["is_download"]
+            )
+            attrs.update(response)
         return attrs
 
     def create(self, validated_data):
