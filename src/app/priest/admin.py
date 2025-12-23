93e8e8f23fed1b0fc98a98c0b626a2ec214d4048
양진혁 <jhyang@yangjinhyeog-ui-MacBookAir.local>
2025-12-23 17:30:30 +0900

    feature: 사제 직책 admin 추가

--- src/app/priest/admin.py
+++ src/app/priest/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 
-from app.priest.models import Priest
+from app.priest.models import Associate, Pastor, Priest
 
 
 @admin.register(Priest)
 class PriestAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["name", "baptismal_name", "is_retired"]
+
+
+@admin.register(Pastor)
+class PastorAdmin(admin.ModelAdmin):
+    list_display = ["name", "baptismal_name", "division", "start_date", "end_date", "is_retired"]
+
+
+@admin.register(Associate)
+class AssociateAdmin(admin.ModelAdmin):
+    list_display = ["name", "baptismal_name", "assistant_system", "start_date", "end_date", "is_retired"]
