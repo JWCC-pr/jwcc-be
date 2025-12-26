9250220cd461b2988dcf0cbcefc62128cd84e766
양진혁 <jhyang@yangjinhyeog-ui-MacBookAir.local>
2025-12-26 16:23:05 +0900

    feature:수도자 및 본당출신 수도자 구분란 nullable

--- src/app/religious/models.py
+++ src/app/religious/models.py
@@ -6,7 +6,7 @@ from app.common.models import BaseModelMixin
 
 class Religious(BaseModelMixin, OrderedModel):
     image = models.ImageField(verbose_name="이미지", max_length=1000, upload_to="religious/image/")
-    category = models.CharField(verbose_name="구분", max_length=50)
+    category = models.CharField(verbose_name="구분", max_length=50, null=True, blank=True)
     name = models.CharField(verbose_name="이름", max_length=20)
     baptismal_name = models.CharField(verbose_name="세례명", max_length=40)
     start_date = models.DateField(verbose_name="재임 시작일")
--- src/app/religious_history/models.py
+++ src/app/religious_history/models.py
@@ -5,7 +5,7 @@ from app.common.models import BaseModelMixin
 
 
 class ReligiousHistory(BaseModelMixin, OrderedModel):
-    category = models.CharField(verbose_name="구분", max_length=50)
+    category = models.CharField(verbose_name="구분", max_length=50, null=True, blank=True)
     name = models.CharField(verbose_name="이름", max_length=20)
     baptismal_name = models.CharField(verbose_name="세례명", max_length=40)
     start_date = models.DateField(verbose_name="재임 시작일")
