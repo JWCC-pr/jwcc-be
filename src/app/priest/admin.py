5229f61feb07c0875274d63e340654e9595f54c0
양진혁 <jhyang@yangjinhyeog-ui-MacBookAir.local>
2025-12-29 12:45:32 +0900

    feature:본당출신 사제, 수도자의 데이터 등록제한 적용

--- src/app/priest/admin.py
+++ src/app/priest/admin.py
@@ -4,4 +4,7 @@ from app.priest.models import Priest
 
 @admin.register(Priest)
 class PriestAdmin(admin.ModelAdmin):
-    pass
+    def has_add_permission(self, request):
+        if Priest.objects.count() >= 3:
+            return False
+        return super().has_add_permission(request)
--- src/app/religious/admin.py
+++ src/app/religious/admin.py
@@ -5,4 +5,7 @@ from app.religious.models import Religious
 
 @admin.register(Religious)
 class ReligiousAdmin(admin.ModelAdmin):
-    pass
+    def has_add_permission(self, request):
+        if Religious.objects.count() >= 3:
+            return False
+        return super().has_add_permission(request)
