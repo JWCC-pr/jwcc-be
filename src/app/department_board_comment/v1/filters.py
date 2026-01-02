164c05825e2cf7e57413869e851450380ac82754
양진혁 <jhyang@yangjinhyeog-ui-MacBookAir.local>
2026-01-02 16:34:10 +0900

    feature:get_comment_parentid

--- src/app/department_board_comment/v1/filters.py
+++ src/app/department_board_comment/v1/filters.py
@@ -4,4 +4,10 @@ from app.department_board_comment.models import DepartmentBoardComment
 
 
 class DepartmentBoardCommentFilter(django_filters.FilterSet):
-    pass
+    parent_id = django_filters.ModelChoiceFilter(label="부모 댓글 ID", queryset=DepartmentBoardComment.objects.all())
+
+    def filter_queryset(self, queryset):
+        queryset = super().filter_queryset(queryset)
+        if self.request.query_params.get("parent_id") is None:
+            queryset = queryset.filter(parent__isnull=True)
+        return queryset
