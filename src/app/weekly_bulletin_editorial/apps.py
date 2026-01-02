ba25534128393186a85d4a144704f4bda5833d64
양진혁 <jhyang@yangjinhyeog-ui-MacBookAir.local>
2026-01-02 17:34:46 +0900

    feature:admin verbose_name수정

--- src/app/weekly_bulletin_editorial/apps.py
+++ src/app/weekly_bulletin_editorial/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class WeeklyBulletinEditorialConfig(AppConfig):
     name = "app.weekly_bulletin_editorial"
-    verbose_name = "주보_7면"
+    verbose_name = "22.주보7면"
 
     def ready(self):
         import app.weekly_bulletin_editorial.signals
