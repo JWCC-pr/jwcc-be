3c58f673ba50e436770cf1f28550affde62a265d
양진혁 <jhyang@yangjinhyeog-ui-MacBookAir.local>
2026-01-02 11:53:03 +0900

    feature:분과 게시글 연관 모델 admin 비활성화

--- src/app/department_board_hit/admin.py
+++ src/app/department_board_hit/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from app.department_board_hit.models import DepartmentBoardHit
 
 
-@admin.register(DepartmentBoardHit)
-class DepartmentBoardHitAdmin(admin.ModelAdmin):
-    pass
+# @admin.register(DepartmentBoardHit)
+# class DepartmentBoardHitAdmin(admin.ModelAdmin):
+#     pass
--- src/app/department_board_like/admin.py
+++ src/app/department_board_like/admin.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
-
-from app.department_board_like.models import DepartmentBoardLike
-
-
-@admin.register(DepartmentBoardLike)
-class DepartmentBoardLikeAdmin(admin.ModelAdmin):
-    pass
+# from django.contrib import admin
+#
+# from app.department_board_like.models import DepartmentBoardLike
+#
+#
+# @admin.register(DepartmentBoardLike)
+# class DepartmentBoardLikeAdmin(admin.ModelAdmin):
+#     pass
