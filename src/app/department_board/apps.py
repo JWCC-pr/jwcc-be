1baec8300477998be74dbc2d342c3a4a613e9d4b
양진혁 <jhyang@yangjinhyeog-ui-MacBookAir.local>
2026-01-05 09:05:10 +0900

    feature:add department board app verbose name

--- src/app/department_board/apps.py
+++ src/app/department_board/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 
 class DepartmentBoardConfig(AppConfig):
     name = "app.department_board"
+    verbose_name = "21.분과게시글"
 
     def ready(self):
         import app.department_board.signals
