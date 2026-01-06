d95745abd83a5a9035359c35a75200c42b340dc9
양진혁 <jhyang@yangjinhyeog-ui-MacBookAir.local>
2026-01-06 09:43:00 +0900

    feature:sub_department_admin_display_id

--- src/app/department/admin.py
+++ src/app/department/admin.py
@@ -7,7 +7,7 @@ from app.sub_department.models import SubDepartment
 
 class SubDepartmentInline(OrderedTabularInline):
     model = SubDepartment
-    fields = ("name", "order", "move_up_down_links")
+    fields = ("id", "name", "order", "move_up_down_links")
     readonly_fields = ("order", "move_up_down_links")
     extra = 0
 
@@ -15,6 +15,6 @@ class SubDepartmentInline(OrderedTabularInline):
 @admin.register(Department)
 class DepartmentAdmin(OrderedInlineModelAdminMixin, admin.ModelAdmin):
     inlines = [SubDepartmentInline]
-    list_display = ["name"]
+    list_display = ["id", "name"]
     search_fields = ["name"]
     search_help_text = "분과명으로 검색하세요."
