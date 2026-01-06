9d11183c6ad459d9f2fa1020f36a7c847bcd587b
양진혁 <jhyang@yangjinhyeog-ui-MacBookAir.local>
2026-01-06 10:52:43 +0900

    feature:model_verbose_name 수정

--- src/app/religious_history/apps.py
+++ src/app/religious_history/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class ReligiousHistoryConfig(AppConfig):
     name = "app.religious_history"
-    verbose_name = "18.본당 출신 수도자"
+    verbose_name = "18.역대 수도자"
 
     def ready(self):
         import app.religious_history.signals
