63d43b9e657202f2758552ab9ac0cf3b824abb87
양진혁 <jhyang@yangjinhyeog-ui-MacBookAir.local>
2026-01-08 12:23:44 +0900

    hotfix:department_board_list_department_required

--- src/app/department_board/v1/filters.py
+++ src/app/department_board/v1/filters.py
@@ -4,7 +4,7 @@ from app.department_board.models import DepartmentBoard
 
 
 class DepartmentBoardFilter(django_filters.FilterSet):
-    department = django_filters.NumberFilter(field_name="department_id", label="분과 ID", required=True)
+    department = django_filters.NumberFilter(field_name="department_id", label="분과 ID")
     sub_department = django_filters.NumberFilter(field_name="sub_department_id", label="세부분과 ID")
     search = django_filters.CharFilter(method="filter_search", label="검색어")
 
